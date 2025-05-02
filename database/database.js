e1f7dc4c258d7fb3b5f2d19a6526f6d21977fdc2
lubuntum <lubuntum@gmail.com>
2025-05-02 11:29:15 +0700

    Добавил новую баззу для smart магазина с заполненной основной инфой

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const sqlite3 = require("sqlite3").verbose()
 
-const db = new sqlite3.Database("./database/db.sqlite3", (err) => {
+const db = new sqlite3.Database("./database/smart_db.sqlite3", (err) => {
     if (err) console.error(err.message)
     else console.log("Connection succeed")
 })
